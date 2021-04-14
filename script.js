a29340fba468c4a928a6703114d73153ac060329
Travisprice08 <75191962+Travisprice08@users.noreply.github.com>
2021-04-13 19:14:02 -0700

    1.9
    Added comment to css file
    
    Created new JS file and copied code from old JS file.

--- script.js
+++ script.js
@@ -1,37 +1,37 @@
-  function newItem () {
-  // Adding a new item to a list
-  let li = $('<li></li>');
-  let inputValue = $('#input').val();
-  li.append(inputValue);
+function newItem () {
+// Adding a new item to a list
+let li = $('<li></li>');
+let inputValue = $('#input').val();
+li.append(inputValue);
 
 
 
-  if (inputValue === '') {
-    alert('You must write something!')
-  } else {
-    $('#list').append(li);
-    //list.appendChild(li);
+if (inputValue === '') {
+  alert('You must write something!')
+} else {
+  $('#list').append(li);
+  //list.appendChild(li);
+}
+
+// Crossing out an item on the list
+function crossOut() {
+    li.addClass('strike');
   }
 
-  // Crossing out an item on the list
-  function crossOut() {
-  		li.addClass('strike');
-  	}
+  li.on('dblclick', function crossOut() {
+    li.addClass('strike');
+  });
 
-    li.on('dblclick', function crossOut() {
-  		li.addClass('strike');
-  	});
+// Adding delete button "X"
 
-  // Adding delete button "X"
+let deleteButton = $('<deleteButton></deleteButton>');
+  deleteButton.append(document.createTextNode('X'));
+  li.append(deleteButton);
 
-  let deleteButton = $('<deleteButton></deleteButton>');
-    deleteButton.append(document.createTextNode('X'));
-    li.append(deleteButton);
-
-    deleteButton.on('click', deleteListItem);
-    function deleteListItem(){
-      li.addClass('delete')
-    }
-    // Makes list sortable
-    $('#list').sortable();
+  deleteButton.on('click', deleteListItem);
+  function deleteListItem(){
+    li.addClass('delete')
   }
+  // Makes list sortable
+  $('#list').sortable();
+}
